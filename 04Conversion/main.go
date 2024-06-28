89e6e6e8657bd5d59d1d4acaaad1ba8985a51bca
Rajatbisht12 <rjatgithub123@gmail.com>
2024-06-28 23:49:34 +0530

    conversion

---
+++ 04Conversion/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println(input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating: ", numRating+1)
+	}
+}
