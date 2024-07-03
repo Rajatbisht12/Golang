26532efc6c3786e2f7a41a41a3b9dbe98f9d2064
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-03 13:12:43 +0530

    loops

---
+++ 14Loops/go.mod
@@ -0,0 +1,3 @@
+module 14loop
+
+go 1.22.2
---
+++ 14Loops/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	days := []string{"Sunday", "Monday", "Saturday", "Tuesday", "Friday"}
+
+	fmt.Println(days)
+
+	// for d := 0; d < len(days); d++ {
+	// 	fmt.Println(days[d])
+	// }
+
+	// for i := range days {
+	// 	fmt.Println(days[i])
+	// }
+
+}
